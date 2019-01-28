01fff4c0c36b9f52e267be587a4a311948fd01bb
冨永将司 <moyashi.harusamen@gmail.com>
2019-01-28 21:19:10 +0900

    イベントの利用

--- chapter1/run.js
+++ chapter1/run.js
@@ -3,5 +3,11 @@ var vm = new Vue({
     data: {
         message: 'Hello Vue!',
         list: ['りんご', 'ばなな', 'すいか']
+    },
+    methods: {
+        handClick: function (e) {
+            console.log(e.target)
+            console.log(this.message)
+        }
     }
 })
