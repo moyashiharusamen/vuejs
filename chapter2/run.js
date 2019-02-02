a9477e36c299f6b86d5c28177155a8a3051d0726
冨永将司 <moyashi.harusamen@gmail.com>
2019-02-02 23:49:06 +0900

    SECTION 10: リストデータの表示と更新

--- chapter2/run.js
+++ chapter2/run.js
@@ -82,3 +82,59 @@ var app7 = new Vue({
         radius: 50
     }
 })
+
+const app8 = new Vue({
+    el: "#app8",
+    data: {
+        name: 'キマイラ',
+        list: [
+            { id: 1, name: 'スライム', hp: 100 },
+            { id: 2, name: 'ゴブリン', hp: 200 },
+            { id: 3, name: 'ドラゴン', hp: 500 }
+        ],
+        text: 'Vue'
+    },
+    methods: {
+        doAdd: function () {
+            const max = this.list.reduce(function (a, b) {
+                return a > b.id ? a : b.id
+            }, 0)
+            this.list.push({
+                id: max + 1,
+                name: this.name,
+                hp: Math.floor(Math.random() * 1000)
+            })
+        },
+        doRemove: function (index) {
+            this.list.splice(index, 1)
+        },
+        rewrite: function () {
+            // this.list[0] = { id: 1, name: 'hoge', hp: 10000 }
+            this.$set(this.list, 0, { id: 1, name: 'hoge', hp: 10000 })
+        },
+        created: function () {
+            this.list.forEach(function (item) {
+                this.$set(item, 'active', false)
+            }, this)
+        },
+        doAttack: function (index) {
+            if (this.list[index].hp > 0) {
+                this.list[index].hp -= 10
+            }
+        }
+    }
+})
+
+// const app9 = new Vue({
+//     el: '#app9',
+//     data: {
+//         list: []
+//     },
+//     created: function () {
+//         axios.get('list.json').then(function (response) {
+//             this.list = response.data
+//         }.bind(this)).catch(function (e) {
+//             console.error(e)
+//         })
+//     }
+// })
