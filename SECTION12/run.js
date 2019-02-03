48c955d1933ceacbf8f9a9bb0022cffffa51a40e
tominaga <moyashi.harusamen@gmail.com>
2019-02-03 19:26:40 +0900

    SECTION 12: テンプレート制御ディレクティブ

---
+++ SECTION12/run.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Vue.js!'
+    }
+})
+
+const app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'Hello <strong>Vue.js!</strong>'
+    }
+})
+
+const app3 = new Vue({
+    el: '#app3',
+    data: {
+        message: 'Vue.js!'
+    }
+})
+
+const app4 = new Vue({
+    el: '#app4',
+    data: {
+        toggle: true
+    }
+})
